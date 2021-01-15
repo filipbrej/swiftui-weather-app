95798732ca7c75d9fd74b3add6ddbf72ffacacbd
Filip Brej <brej.filip@gmail.com>
2021-01-15 00:41:04 -0600

    Initial Commit

---
+++ weather-app-swiftui/weather_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_swiftuiApp.swift
+//  weather-app-swiftui
+//
+//  Created by Filip Brej on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
